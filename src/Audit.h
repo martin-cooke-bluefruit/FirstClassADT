ed2022a320075df1f03a7991be5d7e94849ebb19
Martin Cooke <martin.cooke@bluefruit.co.uk>
2020-09-06 14:53:25 +0100

    removed unused function declaration

--- src/Audit.h
+++ src/Audit.h
@@ -7,6 +7,5 @@ void Audit_RecipeNameChange(char* recipeName, char* newValue);
 void Audit_RecipeVolumeChange(char* recipeName, uint32_t oldValue, uint32_t newValue);
 void Audit_RecipeStartDelayChange(char* recipeName, uint32_t oldValue, uint32_t newValue);
 void Audit_PrintLog(void);
-void Audit_IntToString(int value, char* string);
 
 #endif
